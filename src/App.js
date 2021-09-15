79ba8531d7adcf0d673b4421cf9f8719c232dbc4
Dina <dine7952@gmail.com>
2021-09-16 03:12:19 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import flash from './assessts/bg.mp4'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <video id="myVideo" controls loop={false}>
+        <source src={flash} type="video/mp4" />
+      </video>
+
+      <div className="content">
+        <h1>Happy Birthday</h1>
+      </div>
     </div>
   );
 }
